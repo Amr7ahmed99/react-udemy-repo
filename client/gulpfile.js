729e1a1706f224d8eb77b674f6833ef3cf6a1ebf
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-04-14 12:55:24 +0200

    starter react project

---
+++ client/gulpfile.js
@@ -0,0 +1,10 @@
+//Top Level Functions (Task, src, dest, watch)
+
+const gulp= require('gulp');
+const gulpSass= require('gulp-sass');
+const sass= gulpSass(require('sass'));
+
+
+gulp.task('sass', async function(){
+    gulp.src('src/index.scss').pipe(sass()).pipe(gulp.dest('src/css'));
+});
---
+++ server/server.js
@@ -0,0 +1,4 @@
+//Express: Is an api or framework for node to create apis in a simple way 
+//mongoose: related to mongoDB
+//body-parser: parses the retrieved api(data) from server in JSON format
+//nodemon: watches the server file for automatic update when changes are found
