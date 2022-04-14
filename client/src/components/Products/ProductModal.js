ea82bf2924cf4c15dcc8454aab44e6cf5fe36458
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-04-14 19:29:21 +0200

    product details

---
+++ client/src/components/Products/ProductModal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Modal from 'react-modal'
+import '../../css/Products/Products.css'
+function ProductModal(props) {
+    const {product, closeModal} = props;
+  return (
+    <Modal className="productModal" isOpen= {product} onRequestClose= {closeModal}>
+        <span className= "close-Icon" onClick={closeModal}> &times;</span>
+        <img src={product.imageUrl} alt="" />
+        <p>{product.title}</p>
+        <p>{product.desc}</p>
+        <span>${product.price}</span>
+    </Modal>
+  )
+}
+
+export default ProductModal
